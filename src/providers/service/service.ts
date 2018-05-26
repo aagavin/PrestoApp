d752101dc7add09617bfdd5dbcd22edf58926ca7
aagavin <aagavin@gmail.com>
2018-05-26 13:35:34 -0400

    service

--- src/providers/service/service.ts
+++ src/providers/service/service.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Storage } from "@ionic/storage";
 import { PrestoConstants } from '../../constants/prestoConstants';
+import { SettingsManagerProvider } from "../settings-manager/settings-manager";
 
 
 @Injectable()
@@ -10,17 +10,25 @@ export class ServiceProvider {
 
   private readonly baseUrl = "https://prestocrapper.herokuapp.com";
 
-  constructor(public http: HttpClient, public storage:Storage) {
+  constructor(public http: HttpClient, private settingsManagerProvider: SettingsManagerProvider) {
     console.log('Hello ServiceProvider Provider');
   }
 
   public getBalance(username: string, password: string): Observable<object> {
     let headers = new HttpHeaders()
       .set('username', username)
-      .set('password', password);
-      // .set('mock', 'true');
+      .set('password', password)
+      .set('mock', 'true');
+    
+    const mock = this.settingsManagerProvider.mock;
+
+    if (mock) {
+      headers.append('mock', 'true');
+    }
 
     return this.http.get(`${this.baseUrl}/presto/balance`, {headers});
+
+
   }
 
 }
