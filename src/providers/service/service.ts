14d1fd52a862dc082bbde62b5536dc3a061d9466
aagavin <aagavin@gmail.com>
2018-06-02 16:34:57 -0400

    removed uneeded imports

--- src/providers/service/service.ts
+++ src/providers/service/service.ts
@@ -1,7 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { PrestoConstants } from '../../constants/prestoConstants';
 import { SettingsManagerProvider } from "../settings-manager/settings-manager";
 
 
