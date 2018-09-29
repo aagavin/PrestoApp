4492ffbb0a7d6e3f69fc07e3361277675e418112
aagavin <aagavin@gmail.com>
2018-09-29 13:32:20 -0400

    username

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -10,9 +10,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class DetailsPage {
 
   public userTable: UserTable = {balance: {balance: '', userTable: []}};
+  public userName: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.userTable = navParams.get('userData');
+    this.userName = navParams.get('username');
   }
 
   ionViewDidLoad() {
